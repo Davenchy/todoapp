b44dd6357f31951cc1b4cc8d2b93b03adb750962
Davenchy <firon1222@gmail.com>
2018-08-01 19:59:33 +0200

    add todos page template

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
-});
+// main routes
+require('./routes/main')(app);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
