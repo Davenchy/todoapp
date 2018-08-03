693367e162d53b68b6e20bff4383ac38bc6009e9
Davenchy <firon1222@gmail.com>
2018-08-03 02:29:18 +0200

    add mongodb cloud url, and cloud mode detector

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/todoapp';
+const PORT = process.env.PORT || 3000;
+
+const isCloud = process.env.PORT?true:false;
+// const isCloud = true;
+
+var url = 'mongodb://localhost:27017/todoapp';
+if (isCloud) url = 'mongodb://admin2:admin2123456@ds111492.mlab.com:11492/todoapp';
 
 mongoose.Promise = global.Promise;
 
