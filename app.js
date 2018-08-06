ea23788d4e11f851112de85d4f6c96746d7a621f
Davenchy <firon1222@gmail.com>
2018-08-06 14:40:21 +0200

    the latest update for cors support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 
 const config = require('./config');
@@ -18,29 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
-
-// setup cors
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-type, x-auth, Origin, Accept, Authorization, X-Requested-With'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-        return res.send();
-    }
-
-    next();
-});
 // frontend routes
 app.use(require('./routes/frontend'));
-// todo RESTful api
-app.use('/todos', require('./routes/todoapi'));
-// user RESTful api
-app.use('/users', require('./routes/userapi'));
+// todo RESTful api with cors support
+app.use('/todos', cors(), require('./routes/todoapi'));
+// user RESTful api with cors support
+app.use('/users', cors(), require('./routes/userapi'));
 
 // check route
 app.get('/checker', (req, res) => {
