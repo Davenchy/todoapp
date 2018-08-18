ed7baea1f00417547018d0a23c39d148e0db8bc1
Davenchy <firon1222@gmail.com>
2018-08-18 13:58:27 +0200

    update cors support

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const app = express();
 
 const config = require('./config');
@@ -21,10 +20,20 @@ app.set('view engine', 'ejs');
 
 // frontend routes
 app.use(require('./routes/frontend'));
+
+// cors support
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE, PUT");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 // todo RESTful api with cors support
-app.use('/todos', cors(), require('./routes/todoapi'));
+app.use('/todos', require('./routes/todoapi'));
 // user RESTful api with cors support
-app.use('/users', cors(), require('./routes/userapi'));
+app.use('/users', require('./routes/userapi'));
 
 // check route
 app.get('/checker', (req, res) => {
